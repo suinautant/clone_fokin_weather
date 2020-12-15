5ce6d2ec7eb77f8b66d662fea08d3040e2feacf4
suinautant <suinautant@gmail.com>
2020-12-15 20:57:05 +0900

    add openweather API

--- App.js
+++ App.js
@@ -1,8 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
 import Loading from './Loading';
+import * as Location from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return <Loading />;
+const API_KEY = "80e79aa39b5bbc179274c3e84d943da7";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      );
+      console.log(data);
+  }
+  getLocation = async () => {
+    try {
+      const response = await Location.requestPermissionsAsync();
+      const { 
+        coords: { latitude, longitude } 
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad"); 
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
